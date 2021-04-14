f6e6b640412ad5903f1b403cb0fd00b68cec5d4a
Pytheon <pawelecfilip6@gmail.com>
2021-04-15 01:27:09 +0200

    Update scar.py
    Added skeleton of script

--- cordic_test.srcs/sources_1/new/python/sources/scar.py
+++ cordic_test.srcs/sources_1/new/python/sources/scar.py
@@ -1,2 +1,23 @@
 
 # SCAR - Static C code Analyzer for RISC-V
+
+# Imports:
+import sys
+import json
+# End imports
+
+arguments = sys.argv
+
+f = open('D:\\VivadoProjects\\cordic_test\\RISCV_IPcore\\cordic_test.srcs' +
+         '\\sources_1\\new\\python\\resources\\riscv_instructions.json')
+database = json.load(f)
+f.close()
+
+for object in database:
+    print(object)
+
+with open(arguments[0]) as asm:
+    code = asm.readlines()
+    for line in code:
+        print(line)
+
