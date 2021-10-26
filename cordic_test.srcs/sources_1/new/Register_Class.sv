ae3d6d2089d69f768896a74bc2fb5ecafc92b054
Pytheon <pawelecfilip6@gmail.com>
2021-10-26 21:12:47 +0200

    Adjusted contents of the Register_Class.sv according to changes in top-hierarchy package.

--- cordic_test.srcs/sources_1/new/Register_Class.sv
+++ cordic_test.srcs/sources_1/new/Register_Class.sv
@@ -1,38 +1,44 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: AGH University of Science and Technology
+// Engineer: Filip P.
 // 
 // Create Date: 14.10.2021 22:48:21
-// Design Name: 
+// Design Name: NA
 // Module Name: Register_Class
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
+// Project Name: RISC-V IP Core
+// Target Devices: NA
+// Tool Versions: NA
 // Description: 
 // 
+// This file contains package defining Register class utilised in other modules.
+//
 // Dependencies: 
+//
+// Architecture_AClass package
 // 
 // Revision:
 // Revision 0.01 - File Created
+// Revision 0.5 - Adjusted to changes from Architecture_AClass package
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
 import Architecture_AClass::*;
+
 package Register_Class;
-    class Register extends Architecture_AClass::Architecture;
-        local `rvector(`rvtype) contents;
+    class Register extends `archpkg::Architecture;
+        local `rvector contents;
         
-        function new(input `rvector(`rvtype) _contents);
+        function new(input `rvector _contents);
             this.contents = _contents;
         endfunction
         
-        function `rvector(`rvtype) Read();
+        function `rvector Read();
             return this.contents;
         endfunction
         
-        function Write(input `rvector(`rvtype) writeval);
+        function Write(input `rvector writeval);
             this.contents = writeval;
         endfunction
         
