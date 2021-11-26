831e7a30c896d9f94c831a6ae37d533627f02c3e
Pytheon <pawelecfilip6@gmail.com>
2021-11-26 23:37:37 +0100

    Created new file for another component. Started working on memory class.

---
+++ cordic_test.srcs/sources_1/new/ImmediateGenerator_class.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 26.11.2021 19:05:19
+// Design Name: 
+// Module Name: ImmediateGenerator_class
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ImmediateGenerator_class(
+
+    );
+endmodule
--- cordic_test.srcs/sources_1/new/Memory_Class.sv
+++ cordic_test.srcs/sources_1/new/Memory_Class.sv
@@ -22,6 +22,7 @@
 // Revision 0.01 - File Created
 // Additional Comments:
 // 
+// Misaligned address access supported, but highly discouraged
 //////////////////////////////////////////////////////////////////////////////////
 
 import Architecture_AClass::*;
@@ -34,6 +35,13 @@ import Architecture_AClass::*;
 `define PROGRAM_MEMORY_SIZE_IN_CELLS 1024 // When MemoryCell is 32 bits wide, this translates to 4kB of memory
 
 `define memorycell `mempkg::MemoryCell
+`define MEMORY_INITIAL_VALUE 'h0000_0000
+`define MEMORY_CELL_SIZE_IN_BYTES (($bits(`memorycell))/`BYTE_SIZE)
+`define BYTE_MASK 'h0000_00FF
+`define WORD_MASK 'h0000_FFFF
+
+//// Compilation switch - misaligned memory access support
+`define ALIGNED_MEM_ACCESS // If ALIGNED_MEM_ACCESS is defined, only aligned access to memory is supported
 
 package Memory_Class;
 
@@ -59,5 +67,45 @@ package Memory_Class;
     
     class Memory extends Architecture_AClass::Architecture;
         `_protected MemoryType main_memory;
+        
+        `_public function new();
+            // Does nothing, as in real processors memory is not cleared on startup
+        endfunction
+        
+        `_public function `memorycell Read(input `uint address, input `uint bytes);
+            `uint memcell_remainder = address % `MEMORY_CELL_SIZE_IN_BYTES;
+            `memorycell intermediate_val = `MEMORY_INITIAL_VALUE;
+            unique case(bytes)
+                1: intermediate_val[0] = this.main_memory[address/`MEMORY_CELL_SIZE_IN_BYTES][memcell_remainder];
+                2: 
+                    if(memcell_remainder == 0) 
+                        intermediate_val[0:1] = this.main_memory[address/`MEMORY_CELL_SIZE_IN_BYTES][0:1];
+                    else if(memcell_remainder == 2)
+                        intermediate_val[0:1] = this.main_memory[address/`MEMORY_CELL_SIZE_IN_BYTES][2:3];
+                    else
+                        $error("Misalingned address!");
+                4:
+                    if(memcell_remainder != 0) $error("Misaligned address!");
+                    else intermediate_val = this.main_memory[address/`MEMORY_CELL_SIZE_IN_BYTES];
+                
+                default:
+                    $error("Unimplemented address range used!");
+            endcase
+            // Sign extention / zero-padding will be performed by other function
+            return intermediate_val;
+        endfunction
+        
+        `_public function Write(input `uint address, input `memorycell data);
+            `uint remainder = address % `MEMORY_CELL_SIZE_IN_BYTES;
+            unique case(remainder)
+                0: this.main_memory[address/`MEMORY_CELL_SIZE_IN_BYTES] = data;
+                
+                `ifdef MISALIGNED_MEM_ACCESS
+                    1: 
+                        this.main_memory[address/`MEMORY_CELL_SIZE_IN_BYTES][1:`MEMORY_CELL_SIZE_IN_BYTES - 1] = data[0:`MEMORY_CELL_SIZE_IN_BYTES - 2];
+                        this.main_memory[
+                `endif
+            endcase
+        endfunction
     endclass
 endpackage
