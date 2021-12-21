a7c0b5cd33af5cf8324c121a293c220494052f0e
Filip Pawelec <pawelecfilip6@gmail.com>
2021-12-21 21:52:50 +0100

    Working on ALU testbench classes.

--- cordic_test.srcs/sources_1/new/ALU_Class.sv
+++ cordic_test.srcs/sources_1/new/ALU_Class.sv
@@ -21,42 +21,63 @@
 
 import Architecture_AClass::*;
 
-// Global package alias
+/**
+* Global package alias
+*/
 `define alupkg ALU_Class
 
 `define OPERATION_ENUM_VEC_BITWIDTH 4
 `define ALU_INTERNALS_INITIAL_STATE `NULL_REG_VAL
 `define ALUVECTOR_BITWIDTH (`REGISTER_GLOBAL_BITWIDTH + 1)
 
-// Typedefs aliases
+/**
+* Global type aliases macros.
+* Used throughout the project, aliases to all types in this file.
+*/
 `define aluvector `alupkg::riscv_aluvector
 `define alu_operation_vector `alupkg::riscv_alu_operation_vector
 `define alu_operation_type `alupkg::OperationType
 
+/**
+* Diagnostic log trace adapter macros and ERROR macros.
+* They are aliases to the main DLT_LOG macro. ERROR macros
+* provide self-documented error record.
+*/
+`define DLT_ALU_TITEM_VARDUMP(logging_entity) `DLT_LOG(logging_entity, \
+                                                         " Transaction Item vars dump: ", \
+                                                         {"left_operand=0x%0h ", \
+                                                         "right_operand=0x%0h ", \
+                                                         "operation=0x%0h ", \
+                                                         "outcome=0x%0h"}, \
+                                                         {this.left_operand, \
+                                                         this.right_operand, \
+                                                         this.operation, \
+                                                         this.outcome}) /**< This macro is to be used ONLY from within the transaction item class. */
+
+`define DLT_ALU_INFO(logging_entity, info) `DLT_LOG(logging_entity, info, {}, {}) /**< This macro can be used anywhere within this file. */
+
+`define DLT_ALU_SCOREBOARD(message) `DLT_LOG("ALU Scoreboard", message, \
+                                                         {"left_operand=0x%0h ", \
+                                                         "right_operand=0x%0h ", \
+                                                         "operation=0x%0h ", \
+                                                         "outcome=0x%0h ", \
+                                                         {this.monitor_item.left_operand, \
+                                                         this.monitor_item.right_operand, \
+                                                         this.monitor_item.operation, \
+                                                         this.monitor_item.outcome}) /**< This macro to be used ONLY from within the scoreboard class. */
+
+`define ERROR_OO_ALU_INTERNAL_TRANSACTION_FAILURE " |ERROR 0x00| "
+
 package ALU_Class;
-    /////////////////////////////////
-    // Typedef:
-    //      riscv_aluvector
-    // Info:
-    //      This type represents output from ALU unit and has one extra bit,
-    //      to indicate overflow.
-    /////////////////////////////////
+    /**
+    * This type represents output of ALU unit.
+    * It has one extra bit for indicating overflow.
+    */
     typedef `packed_arr(`rvtype, `ALUVECTOR_BITWIDTH, riscv_aluvector);
 
-    /////////////////////////////////
-    // Typedef:
-    //      riscv_alu_operation_vector
-    // Info:
-    //      This type represents bus used for transmitting ALU unit operations.
-    /////////////////////////////////
-    typedef `packed_arr(`rvtype, `OPERATION_ENUM_VEC_BITWIDTH, riscv_alu_operation_vector);
-
-    /////////////////////////////////
-    // Typedef:
-    //      enum OperationType
-    // Info:
-    //      This type represents possible operations to be performed by the ALU unit.
-    /////////////////////////////////
+    /**
+    * This enum contains operations that ALU can perform.
+    */
     typedef enum `rvtype [`OPERATION_ENUM_VEC_BITWIDTH - 1 : 0]
                         {  ALU_ADD = 1,
                            ALU_SUB = 2,
@@ -87,25 +108,111 @@ package ALU_Class;
     endclass
 
     class AluTransactionItem;
-        `_public `rvector left_operand;
-        `_public `rvector right_operand;
+        `_public rand `rvector left_operand;
+        `_public rand `rvector right_operand;
         `_public `aluvector outcome;
-        `_public `alu_operation_type operation;
+        `_public rand `alu_operation_type operation;
+        /** No constraints needed here. */
+        `_public task log(input string tag="");
+            `DLT_ALU_TITEM_VARDUMP(tag);
+        endtask
     endclass
 
     class AluDriver;
         `_public virtual ALUInterface aluinf;
         `_public mailbox alu_driver_mailbox;
+        `_public mailbox alu_scoreboard_mailbox;
+        `_private AluTransactionItem item;
 
         `_public task run();
-            
+            `DLT_ALU_INFO("ALU Driver", " Starting ALU Driver... ");
+            forever begin
+                `DLT_ALU_INFO("ALU Driver", " Waiting for item... ");
+                this.alu_driver_mailbox.get(this.item);
+                this.item.log("ALU Driver");
+                /**
+                * Drive the interface
+                */
+                this.aluinf.left_operand <= this.item.left_operand;
+                this.aluinf.right_operand <= this.item.right_operand;
+                this.aluinf.operation <= this.item.operation;
+
+                this.alu_scoreboard_mailbox.put(this.item); /**< Put transaction item in mailbox for scoreboard to read. */
+            end // forever loop
+        endtask
+    endclass
+
+    class AluMonitor;
+        `_public virtual ALUInterface aluinf;
+        `_public mailbox alu_scoreboard_mailbox;
+        `_private AluTransactionItem item;
+
+        `_public task run();
+            `DLT_ALU_INFO("ALU Monitor", " Starting ALU Monitor... ");
+            forever begin
+                this.item = new;
+                `DLT_ALU_INFO("ALU Monitor", " Waiting for valid transaction... ");
+                @(`CLOCK_ACTIVE_EDGE this.aluinf.clk); /**< Valid transaction occurs at active clock edge. */
+                /**
+                * Take a snapshot of interface signals and send them to Scoreboard.
+                */
+                this.item.left_operand = this.aluinf.left_operand;
+                this.item.right_operand = this.aluinf.right_operand;
+                this.item.operation = this.aluinf.operation;
+                this.item.outcome = this.aluinf.outcome;
+                this.alu_scoreboard_mailbox.put(this.item);
+                this.item.log("ALU Monitor");
+            end
+        endtask
+    endclass
+
+    class AluScoreborad;
+        `_public mailbox alu_driver_mailbox;
+        `_public mailbox alu_monitor_mailbox;
+        `_private AluTransactionItem driver_item;
+        `_private AluTransactionItem monitor_item;
+
+        `_public task run();
+            `DLT_ALU_INFO("ALU Scoreboard", " Starting ALU Scoreboard... ");
+            forever begin
+                this.alu_driver_mailbox.get(this.driver_item); /**< Wait for the driver to initialise transaction. */
+                this.alu_monitor_mailbox.get(this.monitor_item); /**< Wait for monitor to register transaction. */
+
+                /**
+                * Check if transaction was correct
+                */
+                if(this.driver_item.left_operand != this.monitor_item.left_operand)
+                    `DLT_ALU_SCOREBOARD(`ERROR_OO_ALU_INTERNAL_TRANSACTION_FAILURE);
+                if(this.driver_item.right_operand != this.monitor_item.right_operand)
+                    `DLT_ALU_SCOREBOARD(`ERROR_OO_ALU_INTERNAL_TRANSACTION_FAILURE);
+                if(this.driver_item.operation != this.monitor_item.operation)
+                    `DLT_ALU_SCOREBOARD(`ERROR_OO_ALU_INTERNAL_TRANSACTION_FAILURE);
+
+                unique case (this.monitor_item.operation)
+                    ALU_ADD:
+                        if(this.monitor_item.outcome != (this.monitor_item.left_operand + this.monitor_item.right_operand))
+                            `DLT_ALU_SCOREBOARD(" |ADD FAIL| ");
+                        else `DLT_ALU_SCOREBOARD(" |ADD PASS| ");
+                    ALU_SUB:
+                        if(this.monitor_item.outcome != (this.monitor_item.left_operand - this.monitor_item.right_operand))
+                            `DLT_ALU_SCOREBOARD(" |SUB FAIL| ");
+                        else `DLT_ALU_SCOREBOARD(" |SUB PASS| ");
+                    ALU_XOR: return left_operand ^ right_operand;
+                    ALU_AND: return left_operand & right_operand;
+                    ALU_NOR: return ~(left_operand | right_operand); // TODO: Check if it doesn't cause overflow here!!!
+                    ALU_OR: return left_operand | right_operand;
+                    ALU_SLT: return left_operand < right_operand ? `static_cast_to_regvector(1) : `static_cast_to_regvector(0);
+                    ALU_NAND: return ~(left_operand & right_operand);
+                    default: ;
+                endcase
+            end // forever loop
         endtask
     endclass
 endpackage
 
 interface ALUInterface(input `rvtype clk);
-    `rvector leftOperand;
-    `rvector rightOperand;
+    `rvector left_operand;
+    `rvector right_operand;
     `aluvector outcome;
     `rvtype reset;
     `alu_operation_type operation;
