5b9518935e32c1fe531d84b46c3be627b3aceb77
Filip Pawelec <pawelecfilip6@gmail.com>
2022-01-04 18:44:06 +0100

    Finished adding all instructions planned to be implemented.

--- cordic_test.srcs/sources_1/new/ControlUnit_Class.sv
+++ cordic_test.srcs/sources_1/new/ControlUnit_Class.sv
@@ -54,6 +54,21 @@ package ControlUnit_Class;
         $readmemb(`CONTROL_UNIT_INSTRUCTION_INFO_FILE, this.instruction_info);
     endtask
 
+    `_public function `CONTROL_UNIT_OUTPUT_TYPE ControlUnitMainFunction(input `insvector instruction_raw);
+        `inspkg::Instruction known_type_instruction = this.DecodeType(instruction_raw);
+        if (known_type_instruction != null) begin
+            unique case (known_type_instruction.Format)
+                Rtype: this.DecodeRtype(known_type_instruction);
+                Itype: this.DecodeItype(known_type_instruction);
+                Stype: this.DecodeStype(known_type_instruction);
+                Btype: this.DecodeBtype(known_type_instruction);
+                Utype: this.DecodeUtype(known_type_instruction);
+                Jtype: this.DecodeJtype(known_type_instruction); 
+                default: ; /**< Do nothing */
+            endcase
+        end else $error("Unknown instruction format!");
+    endfunction
+
     `_private function `inspkg::Instruction DecodeType(input `insvector instruction_raw);
         /**
         * Find first memory address at which this opcode occurs
@@ -74,7 +89,6 @@ package ControlUnit_Class;
             ret_obj = inspkg::JTypeInstruction::new(instruction_raw);
         else
             ret_obj = null;
-        
         return ret_obj;
     endfunction
 
@@ -557,14 +571,56 @@ package ControlUnit_Class;
         `uint steering = utypeins.Fields[0].ExtractFromInstr(utypeins.Contents);
         unique case (steering)
             55: begin /**< LUI */
-                
+                this.cuinf.CUJMPCTRL <= 0;
+                this.cuinf.CUBCTRL <= 0;
+                this.cuinf.MUX2 <= 0;
+                this.cuinf.MUX3 <= 0;
+                this.cuinf.MUX4 <= 0;
+                this.cuinf.IMM1 <= `sepkg::SignExtender::ExtendSign(utypeins.Fields[2].ExtractFromInstr, utypeins.Fields[2].ImmBitWidth, 12);
+                this.cuinf.RS1 <= 0; /**< To ensure that zeros are added to extended immediate value */
+                this.cuinf.RD <= utypeins.Fields[1].ExtractFromInstr(utypeins.Contents);
+                this.cuinf.REGW <= 1;
+                this.cuinf.MEMW <= 0;
+                this.cuinf.MEMR <= 1;
+                this.cuinf.ALU0 <= ALU_ADD;
             end 
-            default: 
+            23: begin /**< AUIPC */
+                this.cuinf.CUJMPCTRL <= 0;
+                this.cuinf.CUBCTRL <= 0;
+                this.cuinf.MUX2 <= 0;
+                this.cuinf.MUX3 <= 0;
+                this.cuinf.MUX4 <= 1; /**< Selecting current PC counter value as a left operand */
+                this.cuinf.IMM1 <= `sepkg::SignExtender::ExtendSign(utypeins.Fields[2].ExtractFromInstr, utypeins.Fields[2].ImmBitWidth, 12);
+                this.cuinf.RD <= utypeins.Fields[1].ExtractFromInstr(utypeins.Contents);
+                this.cuinf.REGW <= 1;
+                this.cuinf.MEMW <= 0;
+                this.cuinf.MEMR <= 1;
+                this.cuinf.ALU0 <= ALU_ADD;
+            end
+            default: ; /**< Do nothing */
         endcase
     endfunction
 
     `_private function `CONTROL_UNIT_OUTPUT_TYPE DecodeJtype(input `inspkg::JTypeInstruction jtypeins);
-
+        `uint steering = jtypeins.Fields[0].ExtractFromInstr(jtypeins.Contents);
+        unique case (steering)
+            111: begin /**< JAL */
+                this.cuinf.CUJMPCTRL <= 1;
+                this.cuinf.CUBCTRL <= 0;
+                this.cuinf.MUX1 <= 1;
+                this.cuinf.MUX2 <= 0;
+                this.cuinf.MUX3 <= 1;
+                this.cuinf.MUX4 <= 1;
+                this.cuinf.IMM0 <= 0;
+                this.cuinf.IMM1 <= `sepkg::SignExtender::ConcatentateJTypeImmediate(jtypeins);
+                this.cuinf.RD <= jtypeins.Fields[1].ExtractFromInstr(jtypeins.Contents);
+                this.cuinf.REGW <= 0;
+                this.cuinf.MEMW <= 0;
+                this.cuinf.MEMR <= 0;
+                this.cuinf.ALU0 <= ALU_ADD;
+            end 
+            default: 
+        endcase
     endfunction
     
     endclass
--- cordic_test.srcs/sources_1/new/SignExtender_Class.sv
+++ cordic_test.srcs/sources_1/new/SignExtender_Class.sv
@@ -24,6 +24,7 @@
 `define UPPER_STYPE_IMMEDIATE_STARTBIT 5
 `define STYPE_IMMEDIATE_OVERALL_BITWIDTH 12
 `define BTYPE_IMMEDIATE_OVERALL_BITWIDTH 12
+`define JTYPE_IMMEDIATE_OVERALL_BITWIDTH 20
 
 import Architecture_AClass::*;
 import Instruction_Classes::*;
@@ -33,7 +34,7 @@ package SignExtender_Class;
         static function `rvector ExtendSign(input `rvector imm, input `uint bitwidth, input `uint position);
             `rvector extended = imm;
             extended = extended << position;
-            if (imm[bitwidth - 1] == 1) begin /**< If the MSB of IMM is 1, extend all upper bits to 1 */
+            if ((imm[bitwidth - 1] == 1) && ((bitwidth + position) < `REGISTER_GLOBAL_BITWIDTH)) begin /**< If the MSB of IMM is 1, extend all upper bits to 1 */
                 for (int index = bitwidth + position ; index < `REGISTER_GLOBAL_BITWIDTH ; index = index + 1 ) begin
                     extended[index] = 1;
                 end
@@ -70,7 +71,21 @@ package SignExtender_Class;
             */
             concatentated[11] = btypeins.Fields[1].ExtractFromInstr(btypeins.Contents);
             concatentated[12] = btypeins.Fields[7].ExtractFromInstr(btypeins.Contents);
-            concatentated = SignExtender::ExtendSign(concatentated, `BTYPE_IMMEDIATE_OVERALL_BITWIDTH, 0);
+            concatentated = SignExtender::ExtendSign(concatentated, `BTYPE_IMMEDIATE_OVERALL_BITWIDTH, 0); /**< No offset because offset was already included */
+            return concatentated;
+        endfunction
+
+        static function `rvector ConcatentateJTypeImmediate(input `inspkg::JTypeInstruction jtypeins);
+            /**
+            * Third immediate field of instruction contains the lowest bits of the immediate - 
+            * those bits need to be shifted by one to the left (because unconditional jump
+            * instructions immediates are always a multiple of 2).
+            */
+            `rvector concatentated = (jtypeins.Fields[4].ExtractFromInstr(jtypeins.Contents) << 1);
+            concatentated[11] = jtypeins.Fields[3].ExtractFromInstr(jtypeins.Contents);
+            concatentated += (jtypeins.Fields[2].ExtractFromInstr(jtypeins.Contents) << 12);
+            concatentated[20] = jtypeins.Fields[5].ExtractFromInstr(jtypeins.Contents);
+            concatentated = SignExtender::ExtendSign(concatentated, `JTYPE_IMMEDIATE_OVERALL_BITWIDTH, 0); /**< No offset because offset was already included */
             return concatentated;
         endfunction
     endclass //SignExtender
